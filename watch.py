d6e7c677756c1ced7b624e1e6a12e3696c0b06c9
chadsfatherlali <chadsfather@gmail.com>
2014-03-06 18:16:29 +0100

    Update watch.py

--- watch.py
+++ watch.py
@@ -21,94 +21,121 @@ from watchdog.events import FileSystemEventHandler
 
 
 class get_all_events(FileSystemEventHandler):
-    def __init__(self):
-        self.folderBase = "trunk"
-        self.localPath = "/Users/ssanchez/Desktop/LOCAL/trunk/"
-        self.remotePath = "/Users/ssanchez/Desktop/SAMBA/trunk/"
-        self.user = getpass.getuser().upper()
+     def __init__(self):
+          self.folderBase = "trunk"
+          self.localPath = "/Users/ssanchez/Desktop/LOCAL/trunk/"
+          self.remotePath = "/Users/ssanchez/Desktop/SAMBA/trunk/"
+          self.user = getpass.getuser().upper()
 
-        print "\n\n>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<"
-        print ">>> BIENVENIDO " + self.user + " !! XD"
-        print ">>> SRSYNC: Telecoming Group 2014."
-        print ">>> Dpto. de diseño."
-        print ">>> Version: 1.0."
-        print ">>> E-mail: graficottg@telecoming.com."        
-        print ">>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<"
-        print ">>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<\n\n\n"
-
-
-    def split_path_local(self, path):
-        match = re.search("(?<=\/" + self.folderBase + "\/).*", path)
-        return match.group(0) if match else ""
+          print "\n\n>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<"
+          print ">>> BIENVENIDO " + self.user + " !! XD"
+          print ">>>"
+          print ">>> SRSYNC: Telecoming Group 2014."
+          print ">>> Dpto. de diseño."
+          print ">>> Version: 1.0."
+          print ">>> E-mail: graficottg@telecoming.com."        
+          print ">>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<"
+          print ">>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<\n\n\n"
 
 
-    def on_modified(self, event):
-        if event.is_directory == False: 
-            print "\n>>> " + self.user + " SE HA MODIFICADO: " + event.src_path
-            print ">>> " + time.ctime()
+     def split_path_local(self, path):
+          match = re.search("(?<=\/" + self.folderBase + "\/).*", path)
+          return match.group(0) if match else ""
 
 
-    def on_created(self, event):
-        print "\n>>> " + self.user + " SE HA CREADO: " + event.src_path
-        print ">>> " + time.ctime()
+     def on_modified(self, event):
+          if event.is_directory == False: 
+               print "\n>>> " + self.user + " SE HA MODIFICADO: " + event.src_path
+               print ">>> " + time.ctime()
 
-        sanitize = self.split_path_local(event.src_path)
-        sanitizeLocalPath = self.localPath + sanitize
-        sanitizeRemotePath = self.remotePath + sanitize
-        
-        if event.is_directory == True:
-            if os.path.isdir(sanitizeRemotePath) == False:
-                operacion = os.makedirs(sanitizeRemotePath, 0777)
-        
-        elif event.is_directory == False:
-            if os.path.exists(sanitizeRemotePath) == False:
-                goodPath = self.split_path_local(os.path.dirname(event.src_path))
+
+     def on_created(self, event):
+          print "\n>>> " + self.user + " SE HA CREADO: " + event.src_path
+          print ">>> " + time.ctime()
+
+          sanitize = self.split_path_local(event.src_path)
+          sanitizeLocalPath = self.localPath + sanitize
+          sanitizeRemotePath = self.remotePath + sanitize
+
+          if event.is_directory == True:
+               if os.path.isdir(sanitizeRemotePath) == False:
+                    operacion = os.makedirs(sanitizeRemotePath, 0777)
+
+          elif event.is_directory == False:
+               if os.path.exists(sanitizeRemotePath) == False:
+                    goodPath = self.split_path_local(os.path.dirname(event.src_path))
                 
-                if(os.path.isdir(self.localPath + goodPath) == True) and (os.path.isdir(self.remotePath + goodPath) == False):
+               if(os.path.isdir(self.localPath + goodPath) == True) and (os.path.isdir(self.remotePath + goodPath) == False):
                     operacion = os.makedirs(self.remotePath + goodPath, 0777)
 
-                operacion = shutil.copyfile(sanitizeLocalPath, sanitizeRemotePath)
+               operacion = shutil.copyfile(sanitizeLocalPath, sanitizeRemotePath)
 
 
-    def on_deleted(self, event):
-        print "\n>>> " + self.user + " SE HA BORRADO: " + event.src_path
-        print ">>> " + time.ctime()
+     def on_deleted(self, event):
+          print "\n>>> " + self.user + " SE HA BORRADO: " + event.src_path
+          print ">>> " + time.ctime()
 
 
-    def on_moved(self, event):
-        print "\n>>> " + self.user + " SE HA RENOMBRADO Ó MOVIDO: " + event.src_path
-        print ">>> " + time.ctime()
+     def on_moved(self, event):
+          print "\n>>> " + self.user + " SE HA RENOMBRADO Ó MOVIDO: " + event.src_path
+          print ">>> " + self.user + "                     DESTINO: " + event.dest_path
+          print ">>> " + time.ctime()
 
-        sanitizeFROM = self.split_path_local(event.src_path)
-        sanitizeLocalPathFROM = self.localPath + sanitizeFROM
-        sanitizeRemotePathFROM = self.remotePath + sanitizeFROM
+          carpetasAcrear = []
+          archivosAcrear = []
 
-        sanitizeTO = self.split_path_local(event.dest_path)
-        sanitizeLocalPathTO = self.localPath + sanitizeTO
-        sanitizeRemotePathTO = self.remotePath + sanitizeTO        
+          sanitizeFROM = self.split_path_local(event.src_path)
+          sanitizeLocalPathFROM = self.localPath + sanitizeFROM
+          sanitizeRemotePathFROM = self.remotePath + sanitizeFROM
 
-        ##if(sanitizeRemotePathFROM)
-        # print os.path.exists(sanitizeRemotePathFROM)
+          sanitizeTO = self.split_path_local(event.dest_path)
+          sanitizeLocalPathTO = self.localPath + sanitizeTO
+          sanitizeRemotePathTO = self.remotePath + sanitizeTO
 
-        if os.path.exists(sanitizeRemotePathFROM) and (event.is_directory == False):
-            #shutil.move(sanitizeRemotePathFROM, sanitizeRemotePathTO)
-            print 1
-        
-        elif os.path.exists(sanitizeRemotePathFROM) and (event.is_directory == True):
-            #os.rename(sanitizeRemotePathFROM, sanitizeRemotePathTO)
-            print 2
+          if event.is_directory == True:
+               shutil.move(sanitizeRemotePathFROM, sanitizeRemotePathTO)
+          
+          elif event.is_directory == False:
+               goodPathFROM, goodFileFROM = os.path.split(sanitizeRemotePathFROM)
+               goodPathTO, goodFileTO = os.path.split(sanitizeRemotePathTO)
 
-        # print "\nFROM\n"
-        # print sanitizeFROM
-        # print sanitizeLocalPathFROM
-        print sanitizeRemotePathFROM
+               shutil.move(goodPathFROM, goodPathTO)
+               #shutil.move(sanitizeRemotePathFROM, sanitizeRemotePathTO)
 
-        # print "\nTO\n"
-        # print sanitizeTO
-        # print sanitizeLocalPathTO
-        print sanitizeRemotePathTO
+               # print "MD >>> " + goodPathFROM + " <<<<<>>>>> " + goodPathTO
+               # print "MF >>> " + sanitizeRemotePathFROM + " <<<<<>>>>> " + sanitizeRemotePathTO
 
+          # shutil.move(sanitizeRemotePathFROM, sanitizeRemotePathTO)
 
+          # if (os.path.exists(sanitizeRemotePathFROM) == True) and (event.is_directory == False):
+          #     archivosAcrear.append(sanitizeRemotePathFROM)
+          #     archivosAcrear.append(sanitizeRemotePathTO)
+
+          # elif (os.path.exists(sanitizeRemotePathFROM) == True) and (event.is_directory == True):
+          #     carpetasAcrear.append(sanitizeRemotePathFROM)
+          #     carpetasAcrear.append(sanitizeRemotePathTO)
+
+          # if carpetasAcrear:
+          #     print "\nCC " + carpetasAcrear[0]
+          #     print "CC " + carpetasAcrear[1]
+          #     shutil.move(carpetasAcrear[0], carpetasAcrear[1])
+          #     carpetasAcrear = []
+
+          # if archivosAcrear:
+          #     print "\nAC " + archivosAcrear[0]
+          #     print "AC " + archivosAcrear[1]
+          #     shutil.move(archivosAcrear[0], archivosAcrear[1])
+          #     archivosAcrear = []
+
+          # print "\nFROM\n"
+          # print sanitizeFROM
+          # print sanitizeLocalPathFROM
+          # print sanitizeRemotePathFROM
+
+          # print "\nTO\n"
+          # print sanitizeTO
+          # print sanitizeLocalPathTO
+          # print sanitizeRemotePathTO
 
 
 if __name__ == "__main__":
